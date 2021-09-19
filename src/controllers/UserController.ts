5efc42cfa21cfd270128cf1a185ada53feb7d89d
wesdralima <wesdralima@gmail.com>
2021-09-18 23:10:06 -0300

    subindo o projeto

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,10 @@
+import { User } from '@models/User';
+
+class UserController {
+  show() {
+    const user = new User();
+    //console.log(this.user.nome);
+  }
+}
+
+export default UserController;
---
+++ src/models/User.ts
@@ -0,0 +1,5 @@
+export default class User {
+    nome:string;
+
+    email:string;
+}
